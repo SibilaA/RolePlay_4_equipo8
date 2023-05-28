f7735f96bd0fb243350263042797e11caaa5b263
SibilaA <sibila.araujo@correo.ucu.edu.uy>
2023-05-28 17:03:19 -0300

    Ejemplo en program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -15,6 +15,22 @@ namespace Program
             scenario.Setup();
             scenario.Run();
             */
+
+            Character p1 = new Elf("Link");
+            GuanteDeGemas guante1 = new GuanteDeGemas();
+            guante1.AddGema(new GemaRosa());
+            p1.AddItem(guante1);
+
+            Character p2 = new Dwarves("Ganon");
+            p2.AddItem(new Espada());
+
+            ConsoleReporter console1 = new ConsoleReporter();
+            AttackEncounter e1 = new AttackEncounter(p1, p2);
+            e1.Reporter = console1;
+            e1.DoEncounter();
+            Character g1 = e1.GetGanador();
+            Console.WriteLine($"El ganador es {g1.Name}");
+
         }
     }
 }

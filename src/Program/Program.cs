f9561c1c1f6a16d37bec5d941fe96d4f0a28c68f
TadeoKi <tadeo.ki@correo.ucu.edu.uy>
2023-05-29 11:46:29 -0300

    Agregue un segundo combate y la final.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -15,7 +15,7 @@ namespace Program
             scenario.Setup();
             scenario.Run();
             */
-
+            //Primer Encuentro
             Character p1 = new Elf("Link");
             GuanteDeGemas guante1 = new GuanteDeGemas();
             guante1.AddGema(new GemaRosa());
@@ -30,6 +30,29 @@ namespace Program
             e1.DoEncounter();
             Character g1 = e1.GetGanador();
             Console.WriteLine($"El ganador es {g1.Name}");
+
+            //Segundo Encuentro
+            Character p3 = new Wizard("Gandalf");
+            p3.AddItem(new BastonMagico());
+            p3.AddItem(new Magic());
+
+            Character p4 = new Troll("Olog-Hai");
+
+            ConsoleReporter console2 = new ConsoleReporter();
+            AttackEncounter e2 = new AttackEncounter(p3, p4);
+            e2.Reporter = console2;
+            e2.DoEncounter();
+            Character g2 = e2.GetGanador();
+            Console.WriteLine($"El ganador es {g2.Name}");
+
+            //Final
+
+            ConsoleReporter console3 = new ConsoleReporter();
+            AttackEncounter e3 = new AttackEncounter(g1, g2);
+            e3.Reporter = console3;
+            e3.DoEncounter();
+            Character g3 = e3.GetGanador();
+            Console.WriteLine($"El campeon de la Tierra media es {g3.Name}");
 
         }
     }
